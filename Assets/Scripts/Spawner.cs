0596f7a4c33c237a0269777a6ba5ef8f9d5bcdc2
Leandro Dal Canto <leandrodalcanto@gmail.com>
2019-02-21 18:01:42 +0100

    Created plane, started canvas and Spawner script

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject cube;
+    public int numberOfCubes;
+    public int min, max;
+
+    private void Start()
+    {
+        PlaceCubes();
+    }
+
+    void PlaceCubes()
+    {
+        for(int i = 0; i < numberOfCubes;i++)
+        {
+            Instantiate(cube, GeneratedPosition(), Quaternion.identity);
+        }
+    }
+
+    Vector3 GeneratedPosition()
+    {
+        int x, y, z;
+        x = Random.Range(min, max);
+        y = Random.Range(min, max);
+        z = Random.Range(min, max);
+        return new Vector3(x, y, z);
+    }
+}
